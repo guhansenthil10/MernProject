cc616615196396a787366c8d4be78c8f956f847b
Guhan Senthil <guhans21ece@srishakthi.ac.in>
2024-01-23 10:16:43 +0530

    updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/users
-//@desc Test route
+const User = require('../../models/User');
+// @route POST api/users
+//@desc Register user
 //@access Public
-router.get('/', (req, res) =>res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
 
+    const {name, email, password} = req.body;
+
+    try {
+        res.send('User route');
+
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    // See if user exists
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+
+});
 module.exports = router;
